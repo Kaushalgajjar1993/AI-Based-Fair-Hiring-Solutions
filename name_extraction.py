98e798fd0b3aa7cf669795416e902dc66f8781b6
Kaushalgajjar1993 <kaushalgajjar52@gmail.com>
2024-09-11 12:52:37 +0100

    Delete name_extraction.py

--- name_extraction.py
+++
@@ -1,8 +0,0 @@
-
-import re
-
-def extract_name(resume_text):
-    name_match = re.search(r'Name:\s*(\w+\s\w+)', resume_text)
-    if name_match:
-        return name_match.group(1)
-    return "Name not found"
