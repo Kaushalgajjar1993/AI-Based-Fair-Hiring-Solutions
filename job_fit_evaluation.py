1fd839b6d4a9373090ecf900bb235c03462556f7
Kaushalgajjar1993 <kaushalgajjar52@gmail.com>
2024-09-11 12:52:10 +0100

    Delete job_fit_evaluation.py

--- job_fit_evaluation.py
+++
@@ -1,5 +0,0 @@
-
-def evaluate_job_fit(resume_text):
-    required_skills = ['Python', 'Java', 'C++', 'SQL']
-    has_required_skills = all(skill in resume_text for skill in required_skills)
-    return "Good fit" if has_required_skills else "Not a good fit"
